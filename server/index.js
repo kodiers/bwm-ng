86df16fc59d666002fe542e7e8f9c61afc9b540e
Victor Yamchinov <kodiers@gmail.com>
2018-09-20 05:00:48 +0300

    Add rental model

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+const Rental = require('./models/rental');
+
+mongoose.connect(config.DB_URI, {
+  "auth": { "authSource": config.authSource },
+  "user": config.user,
+  "pass": config.pass
+});
+
+const app = express();
+
+app.get('/rentals', function (req, res) {
+  res.json({'success': true});
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, function () {
+  console.log('I am running');
+});

4cbc89455bf7d596d2f121e472aaaedceae058d2
Victor Yamchinov <kodiers@gmail.com>
2018-09-26 04:13:38 +0300

    Styling rental-details

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -3,11 +3,14 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 
+import {NgPipesModule} from 'ngx-pipes';
+
 import {RentalListComponent} from './rental-list/rental-list.component';
 import {RentalListItemComponent} from './rental-list-item/rental-list-item.component';
 import {RentalComponent} from './rental.component';
 import {RentalService} from './shared/rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
+import {UppercasePipe} from '../common/pipes/uppercase.pipe';
 
 
 const routes: Routes = [
@@ -23,12 +26,14 @@ const routes: Routes = [
     RentalComponent,
     RentalListComponent,
     RentalListItemComponent,
-    RentalDetailComponent
+    RentalDetailComponent,
+    UppercasePipe
   ],
   imports: [
     CommonModule,
     HttpClientModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    NgPipesModule
   ],
   providers: [
     RentalService

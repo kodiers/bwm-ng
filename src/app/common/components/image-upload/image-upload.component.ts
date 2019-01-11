27de1c901010c41780f4503c4b5b54c38268587e
Victor Yamchinov <kodiers@gmail.com>
2019-01-11 05:20:45 +0300

    completed image upload

--- src/app/common/components/image-upload/image-upload.component.ts
+++ src/app/common/components/image-upload/image-upload.component.ts
@@ -1,8 +1,13 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Output, ViewContainerRef} from '@angular/core';
+import {HttpErrorResponse} from '@angular/common/http';
+
+import {ToastsManager} from 'ng2-toastr';
 
 import {ImageUploadService} from './image-upload.service';
 
 class FileSnippet {
+  static readonly IMAGE_SIZE = {width: 950, height: 720};
+
   pending = false;
   status = 'INIT';
 
@@ -17,38 +22,85 @@ class FileSnippet {
 export class ImageUploadComponent {
   @Output() imageUploaded = new EventEmitter();
   @Output() imageError = new EventEmitter();
+  @Output() imageLoadedToContainer = new EventEmitter();
+  @Output() croppingCancel = new EventEmitter();
 
   selectedFile: FileSnippet;
+  imageChangedEvent: any;
 
-  constructor(private imageService: ImageUploadService) { }
+  constructor(private imageService: ImageUploadService,
+              private toastr: ToastsManager,
+              private vcr: ViewContainerRef) {
+    this.toastr.setRootViewContainerRef(vcr);
+  }
 
   private onSuccess(imageUrl: string) {
     this.selectedFile.pending = false;
     this.selectedFile.status = 'OK';
+    this.imageChangedEvent = null;
     this.imageUploaded.emit(imageUrl);
   }
 
   private onFailure() {
     this.selectedFile.pending = false;
     this.selectedFile.status = 'FAIL';
+    this.imageChangedEvent = null;
     this.imageError.emit('');
   }
 
-  processFile(imageInput) {
-    const file: File = imageInput.files[0];
-    const reader = new FileReader();
-    reader.addEventListener('load', (event: any) => {
-      this.selectedFile = new FileSnippet(event.target.result, file);
-      this.selectedFile.pending = true;
-      this.imageService.uploadImage(this.selectedFile.file).subscribe(
-        (imageUrl: string) => {
-          this.onSuccess(imageUrl);
-        },
-        () => {
-          this.onFailure();
-        });
-    });
-    reader.readAsDataURL(file);
+  imageCropped(file: File): FileSnippet | File {
+    if (this.selectedFile) {
+      return this.selectedFile.file = file;
+    }
+    return this.selectedFile = new FileSnippet('', file);
+  }
+
+  imageLoaded() {
+    this.imageLoadedToContainer.emit();
+  }
+
+  cancelCropping() {
+    this.imageChangedEvent = null;
+    this.croppingCancel.emit();
+  }
+
+  processFile(event) {
+    this.selectedFile = undefined;
+    const URL = window.URL;
+    let file, img;
+    if ((file = event.target.files[0]) && (file.type === 'image/png' || file.type === 'image/jpeg')) {
+      img = new Image();
+      const self = this;
+      img.onload = function () {
+        if (this.width > FileSnippet.IMAGE_SIZE.width && this.height > FileSnippet.IMAGE_SIZE.height) {
+          self.imageChangedEvent = event;
+        } else {
+          self.toastr.error(`Min width is ${FileSnippet.IMAGE_SIZE.width} and min height ${FileSnippet.IMAGE_SIZE.height}`, 'Error');
+        }
+      };
+      img.src = URL.createObjectURL(file);
+    } else {
+      this.toastr.error('Unsupported file type. Only JPEG and PNG is allowed!', 'Error');
+    }
+  }
+
+  uploadImage() {
+    if (this.selectedFile) {
+      const reader = new FileReader();
+      reader.addEventListener('load', (event: any) => {
+        this.selectedFile.src = event.target.result;
+        this.selectedFile.pending = true;
+        this.imageService.uploadImage(this.selectedFile.file).subscribe(
+          (imageUrl: string) => {
+            this.onSuccess(imageUrl);
+          },
+          (errorResponse: HttpErrorResponse) => {
+            this.toastr.error(errorResponse.error.errors[0].detail, 'Error');
+            this.onFailure();
+          });
+      });
+      reader.readAsDataURL(this.selectedFile.file);
+    }
   }
 
 }
--- src/app/common/components/image-upload/image-upload.module.ts
+++ src/app/common/components/image-upload/image-upload.module.ts
@@ -1,13 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
+import { ImageCropperModule } from 'ngx-image-cropper';
+
 import {ImageUploadComponent} from './image-upload.component';
 import {ImageUploadService} from './image-upload.service';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ImageCropperModule
   ],
   exports: [
     ImageUploadComponent
